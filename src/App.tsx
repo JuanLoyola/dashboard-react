65665162b5bfbaebcd1b9afb9d505600dab2ff40
Juan Loyola <juanjoloyola@hotmail.com>
2024-04-15 00:59:13 -0300

    coingecko

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,78 @@
-import './App.css'
-import { Card, CategoryBar, Divider } from '@tremor/react'
-import { CardPrice } from './components/CardPrice'
-import { Chart } from './components/Chart'
-import { PrList } from './components/PrList'
-import { TableComponent } from './components/Table'
-import AmountList from './components/AmountList'
-import { TrackerComponent } from './components/Tracker'
+import './App.css';
+import { Card, CategoryBar, Divider } from '@tremor/react';
+import { CardPrice, CryptoData } from './components/CardPrice';
+import { Chart } from './components/Chart';
+import { PrList } from './components/PrList';
+import { TableComponent } from './components/Table';
+import AmountList from './components/AmountList';
+import { TrackerComponent } from './components/Tracker';
+import { useEffect, useState } from 'react';
+
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
+
+type DataState = {
+  btcChange: CryptoData | null;
+  ethChange: CryptoData | null;
+  usdtChange: CryptoData | null;
+  xrpChange: CryptoData | null;
+};
 
 function App() {
+  const [data, setData] = useState<DataState>({
+    btcChange: null,
+    ethChange: null,
+    usdtChange: null,
+    xrpChange: null,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+
+      const btcData = jsonData.find((item: CryptoData) => item.id === 'bitcoin');
+      const ethData = jsonData.find((item: CryptoData) => item.id === 'ethereum');
+      const usdtData = jsonData.find((item: CryptoData) => item.id === 'tether');
+      const xrpData = jsonData.find((item: CryptoData) => item.id === 'ripple');
+
+      setData({
+        btcChange: btcData || null,
+        ethChange: ethData || null,
+        usdtChange: usdtData || null,
+        xrpChange: xrpData || null,
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  const cryptocurrencies = [
+    { title: 'BTC', key: 'btcChange' },
+    { title: 'ETH', key: 'ethChange' },
+    { title: 'USDT', key: 'usdtChange' },
+    { title: 'XRP', key: 'xrpChange' },
+  ];
 
   return (
     <>
       <div className='container mx-auto py-10 px-2 grid gap-4'>
-        <div className='flex justify-start items-start gap-x-2'>
-          <CardPrice title='BTC' amount={3.145} img='btc' />
-          <CardPrice title='ETH' amount={-127.58} img='eth' />
+        <div className='grid grid-cols-2 xl:grid-cols-4 gap-x-4 gap-y-4'>
+          {cryptocurrencies.map((crypto) => {
+            const changeData = data[crypto.key as keyof DataState];
+
+            if (changeData) {
+              return (
+                <CardPrice
+                  key={crypto.key}
+                  title={crypto.title}
+                  price_change_percentage_24h={changeData.price_change_percentage_24h}
+                  image={changeData.image}
+                />
+              );
+            }
+
+            return null;
+          })}
         </div>
 
         <Divider />
@@ -45,7 +103,7 @@ function App() {
         <TableComponent />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/CardPrice.tsx
+++ src/components/CardPrice.tsx
@@ -1,33 +1,26 @@
-import { RiEthLine, RiBtcLine } from '@remixicon/react';
 import { Card } from '@tremor/react';
 
-type CardPriceProps = {
-    title: string;
-    amount: number;
-    img: 'btc' | 'eth'
+export type CryptoData = {
+    title: string
+    price_change_percentage_24h: number;
+    image?: string;
+    id?: string
 }
 
-const iconMapping = {
-    btc: RiBtcLine,
-    eth: RiEthLine,
-};
-
-export function CardPrice({ title, amount, img }: CardPriceProps) {
-    const IconComponent = iconMapping[img];
-
+export function CardPrice({ title, price_change_percentage_24h, image }: CryptoData) {
     return (
         <Card
             className="flex gap-x-4 items-center justify-center"
             decoration="top"
             decorationColor={title == 'BTC' ? 'yellow' : 'indigo'}
         >
-            {IconComponent && <IconComponent className="text-white" size={60} />}
+            <img className='h-12 w-12 rounded-full' src={image} alt={title} />
 
             <span className="grid">
                 <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">{title}</p>
 
-                <p className={`xl:text-3xl font-semibold ${amount > 0 ? 'text-green-500' : amount < 0 ? 'text-red-500' : 'text-tremor-content-strong dark:text-dark-tremor-content-strong'}`}>
-                    ${amount}
+                <p className={`xl:text-3xl font-semibold ${price_change_percentage_24h > 0 ? 'text-green-500' : price_change_percentage_24h < 0 ? 'text-red-500' : 'text-tremor-content-strong dark:text-dark-tremor-content-strong'}`}>
+                    {price_change_percentage_24h}%
                 </p>
             </span>
         </Card>
